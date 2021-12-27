ab884e3067efe2e1795b9883d84df3e16421d69e
Alex. Z <azavgo@yahoo.com>
2021-12-27 17:24:36 +1100

    Added random number generator

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-//uuid v4 format c69436a1975fe2ebb0a6b09163031397 - 32 hex numbers, or 16 double hex numbers
-//call: https://qrng.anu.edu.au/API/jsonI.php?length=16&type=hex16&size=1
-//{"type":"string","length":16,"size":1,"data":["f1","65","6e","95","77","5e","5d","3c","36","13","34","63","27","8d","81","83"],"success":true}
-
-async function newUUID() {
-    let data = await fetch("https://qrng.anu.edu.au/API/jsonI.php?length=16&type=hex16&size=1") 
-                    .then(response => response.json())                
-                    .then(json => { return json.data; });
-    
-    
-    // If the 1st digit has to be > 0, uncomment the following code
-    /*
-    let data_0 = parseInt("0x" + data[0]);
-    while (data_0 < 16) {
-        data = await fetch("https://qrng.anu.edu.au/API/jsonI.php?length=16&type=hex16&size=1") 
-        .then(response => response.json())                
-        .then(json => { return json.data; });
-
-        data_0 = parseInt("0x" + data[0]);   
-    } 
-    */
-    document.getElementById("uuidOutput").innerHTML = data.join('');
-} 
+const newUUID = async () => {
+    try {
+        const res = await fetch(`https://qrng.anu.edu.au/API/jsonI.php?length=16&type=hex16&size=1`); 
+        const data = await res.json(); 
+        const uuid = data.data.join('');
+        document.getElementById("uuidOutput").innerHTML = uuid;
+    } catch (error) {
+        //console.error(error);
+        const genRanHex = (size) => [...Array(size)]
+                            .map(() => Math.floor(Math.random() * 16)
+                            .toString(16))
+                            .join('');
+        const uuid = genRanHex(32);
+        document.getElementById("uuidOutput").innerHTML = uuid;
+    }
+}
 
 
     
